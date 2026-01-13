4686cd7d314f9c6e87ba909f0434e8ec79a370e1
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-01-14 01:38:00 +0500

    initialize Ecom-webApp project with Maven and Spring Boot setup

---
+++ src/test/java/org/hashlearning/ecomwebapp/EcomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hashlearning.ecomwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
