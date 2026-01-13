f2edfad375177b15991cfb6f00200911acd9b80f
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-01-14 01:58:27 +0500

    add database configuration and create Product model and controller

---
+++ src/main/java/org/hashlearning/ecomwebapp/controllers/ProductController.java
@@ -0,0 +1,5 @@
+package org.hashlearning.ecomwebapp.controllers;
+
+
+public class ProductController {
+}
---
+++ src/main/java/org/hashlearning/ecomwebapp/model/Product.java
@@ -0,0 +1,31 @@
+package org.hashlearning.ecomwebapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+}
