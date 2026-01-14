dccf35722902ec5b5877a288ca2ffa88851e884a
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-01-14 17:17:00 +0500

    add functionality to upload products with images in ProductController and ProductService

--- src/main/java/org/hashlearning/ecomwebapp/controllers/ProductController.java
+++ src/main/java/org/hashlearning/ecomwebapp/controllers/ProductController.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -30,5 +32,16 @@ public class ProductController {
             return new ResponseEntity<>(product, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product product1 = null;
+        try {
+            product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/org/hashlearning/ecomwebapp/service/ProductService.java
+++ src/main/java/org/hashlearning/ecomwebapp/service/ProductService.java
@@ -4,7 +4,9 @@ import org.hashlearning.ecomwebapp.model.Product;
 import org.hashlearning.ecomwebapp.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -17,6 +19,14 @@ public class ProductService {
     }
 
     public Product getProductById(int id) {
-        return repo.findById(id).orElse(null);
+        return repo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addProduct(Product data, MultipartFile image) throws IOException {
+        data.setImageName(image.getOriginalFilename());
+        data.setImageType(image.getContentType());
+            data.setImageData(image.getBytes());
+
+        return repo.save(data);
     }
 }
